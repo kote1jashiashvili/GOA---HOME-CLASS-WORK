c7fa88922a060d68f605c28841503ef7b89be191
kote1jashiashvili <kotejashiashvili2013@gmail.com>
2024-09-13 19:13:26 +0400

    commit

---
+++ Level 36/classwork/36.py
@@ -0,0 +1,9 @@
+name = input("enter your name : ")
+surname = input("enter your surname : ")
+age = int(input("enter your age : "))
+sister_or_brother = input("do you have sister or brother? : ")
+family_member = input("enter one of your family member : ")
+list_of_user = [name, surname, age, sister_or_brother, family_member]
+
+print(list_of_user)
+print("succsesfully joined your account")
---
+++ Level 36/homework/36.py
@@ -0,0 +1,3 @@
+list_of_kotes_family = ["mariami-sister", "kote-me", "maiko-mom", "miro-dad"]
+print(list_of_kotes_family[1])
+print("kote is at 1st place bc programing starts with 0")

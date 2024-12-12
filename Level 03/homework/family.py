7241da71a1cc7cb1226c2008773ef4d9f7080033
kote1jashiashvili <166549448+kote1jashiashvili@users.noreply.github.com>
2024-12-12 20:30:04 +0400

    Delete Level 03 directory

--- Level 03/homework/family.py
+++
@@ -1,9 +0,0 @@
-me = "kote jashiashvili"
-sister = "mariam jashiashvili"
-mom = "maiko kirvalidze"
-dad = "miro jashiashvili"
-
-
-
-
-print( "in my family, there are 4 people. me, my sister, my mom and my dad. ""i am " + me + " my sister is " + sister + " my mom is " + mom + " my dad is " + dad)

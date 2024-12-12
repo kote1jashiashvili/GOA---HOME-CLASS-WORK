31d4ff7ea1a9ee07924ddf7f3420113225e46256
kote1jashiashvili <166549448+kote1jashiashvili@users.noreply.github.com>
2024-12-12 20:29:19 +0400

    Delete Level 01 directory

--- Level 01/homework/01.py
+++
@@ -1,7 +0,0 @@
-name = "kote"
-surname = "jashiashvili"
-age = 10
-country = "georgia"
-city = "tbilisi"
-
-print( "my name is " + name + ". my surname is " + surname + ". i am " + str(age) + " years old" + ". i live in " + country , city + ".")
